b8a645566f339c176363f72b19558d2a62baaf13
thzt <thzt.xx@gmail.com>
2018-01-31 15:35:04 +0800

    update

--- index.js
+++ index.js
@@ -5,8 +5,54 @@ const log = (...args) => chrome.extension.sendRequest({
   args,
 });
 
-// 注册回调，每一个http请求响应后，都触发该回调
-chrome.devtools.network.onRequestFinished.addListener(async (...args) => {
+// 用于串行化处理promise
+// 尤其是前一个promise还没resolved，后一个promise就已经出现的时候
+const PromiseExecutor = class {
+  constructor() {
+    // lazy promise队列
+    this._queue = [];
+
+    // 一个变量锁，用来控制当前是否要执行队列中的lazy promise
+    this._isBusy = false;
+  }
+
+  each(callback) {
+    this._callback = callback;
+  }
+
+  // 通过isBusy实现加锁
+  // 如果当前有任务正在执行，就返回，否则就按队列中任务的顺序来执行
+  add(lazyPromise) {
+    this._queue.push(lazyPromise);
+
+    if (this._isBusy) {
+      return;
+    }
+
+    this._isBusy = true;
+
+    // execute是一个async函数，执行后立即返回，返回一个promise
+    // 因此，add可以在execute内的promise resolved之前再次执行
+    this.execute();
+  };
+
+  async execute() {
+
+    // 按队列中的任务顺序来依次执行
+    while (this._queue.length !== 0) {
+      const head = this._queue.shift();
+      const value = await head();
+      this._callback && this._callback(value);
+    }
+
+    // 执行完之后，解锁
+    this._isBusy = false;
+  };
+};
+
+const executor = new PromiseExecutor;
+
+const handleHttp = async args => {
   try {
     const [{
       // 请求的类型，查询参数，以及url
@@ -25,4 +71,13 @@ chrome.devtools.network.onRequestFinished.addListener(async (...args) => {
   } catch (err) {
     log(err.stack || err.toString());
   }
-});
+};
+
+// 注册回调，每一个http请求响应后，都触发该回调
+chrome.devtools.network.onRequestFinished.addListener(
+  (...args) =>
+    executor.add(
+      (
+        args => () => handleHttp(args)
+      )(args))
+);
